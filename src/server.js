2e7328d94880ef20fc74e6e5823a677a6bfcb923
Bridget McErlean <bridgetmcerlean@gmail.com>
2015-11-13 21:58:40 +0000

    Make test pass by starting the server instance.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
+var express = require('express');
+
 module.exports = function() {
+    var app = express();
+
     return new Promise(function(resolve, reject) {
-        resolve();
+        var server = app.listen(8000, function() {
+            resolve(server);
+        });
     });
 };
