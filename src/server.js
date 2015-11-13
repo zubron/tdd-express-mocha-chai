9cc170d9f06e22f50592c37846c609e0fccca9e0
Bridget McErlean <bridgetmcerlean@gmail.com>
2015-11-13 23:04:15 +0000

    Teach server to send JSON response

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ module.exports = function(port) {
     var app = express();
 
     app.post('/messages', function (req, res) {
-        res.sendStatus(201);
+        res.status(201).send({});
     });
 
     return new Promise(function(resolve, reject) {
