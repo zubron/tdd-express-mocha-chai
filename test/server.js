5fff1527c9f49d25dc3f575602cf34dc3f65cd97
Bridget McErlean <bridgetmcerlean@gmail.com>
2015-11-13 22:23:12 +0000

    Add failing test for accepting POST requests to /messages

--- test/server.js
+++ test/server.js
@@ -1,9 +1,11 @@
 var createMessageServer = require('../src/server');
 var expect = require('chai').expect;
+var rp = require('request-promise');
 
 describe('Message Server', function() {
     var messageServer;
     var port = 8080;
+    var url = 'http://localhost:' + port;
 
     beforeEach(function() {
         return createMessageServer(port)
@@ -22,8 +24,26 @@ describe('Message Server', function() {
         it('should exist', function() {
             expect(messageServer).to.exist;
         });
+
         it('should be listening on a specified port', function() {
             expect(messageServer.address().port).to.equal(port);
+        });
+
+        it('should accept POST requests to /messages', function() {
+            var request = {
+                method: 'POST',
+                json: true,
+                uri: url + '/messages',
+                resolveWithFullResponse: true,
+                body: {
+                    message: 'Cats are cute'
+                }
+            };
+
+            return rp(request)
+                .then(function(response) {
+                    expect(response.statusCode).to.not.equal(404);
+                });
         });
     });
 });
