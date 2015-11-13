2033adb2bbed0182337d7f27d7cdadd72698d677
Bridget McErlean <bridgetmcerlean@gmail.com>
2015-11-13 22:25:11 +0000

    Teach server to accept POST requests to /messages

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ var express = require('express');
 module.exports = function(port) {
     var app = express();
 
+    app.post('/messages', function (req, res) {
+        res.sendStatus(200);
+    });
+
     return new Promise(function(resolve, reject) {
         var server = app.listen(port, function() {
             resolve(server);
