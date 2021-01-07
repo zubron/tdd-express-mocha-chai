5960ccfd0b3f036b6b8ce0f48b1a2dbb08e34809
Rophy Tsai <rophyt@amazon.com>
2021-01-07 13:03:53 +0800

    update deploy script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-rm -rf /home/ubuntu/mobile-backend
-mkdir -p /home/ubuntu/mobile-backend
-cd /home/ubuntu/mobile-backend
+set -e
+
+TMP_DIR=/tmp/mobile-backend-$(date +%s)
+mkdir -p "$TMP_DIR"
+pushd "$TMP_DIR"
 tar -zxf /tmp/mbackend.tar.gz
+popd
+
+mv /home/ubuntu/mobile-backend /home/ubuntu/mobile-backend-old && mv "$TMP_DIR" /home/ubuntu/mobile-backend
+rm -rf /home/ubuntu/mobile-backend-old
