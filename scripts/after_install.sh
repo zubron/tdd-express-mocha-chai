efc913c23b7185f1cf6b0162d50d368378d46079
Rophy Tsai <rophyt@amazon.com>
2021-01-07 14:00:52 +0800

    fix deployment scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+# working directory should be something like:
+# /opt/codedeploy-agent/deployment-root/3820674c-32d1-448e-bdd3-dc57d69af250/d-GMIE7G0B7/deployment-archive/
+
 TMP_DIR=/tmp/mobile-backend-$(date +%s)
 mkdir -p "$TMP_DIR"
-pushd "$TMP_DIR"
-tar -zxf /tmp/mbackend.tar.gz
-popd
-
+tar -zxf mbackend.tar.gz -C "$TMP_DIR"
 mv /home/ubuntu/mobile-backend /home/ubuntu/mobile-backend-old && mv "$TMP_DIR" /home/ubuntu/mobile-backend
 rm -rf /home/ubuntu/mobile-backend-old
+
+
