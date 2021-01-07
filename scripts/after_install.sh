9ed08c16201189122d9bbf8139c0c7ec38c7e637
Rophy Tsai <rophyt@amazon.com>
2021-01-07 12:43:04 +0800

    update appspec and scripts

---
+++ scripts/after_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf /home/ubuntu/mobile-backend
+mkdir -p /home/ubuntu/mobile-backend
+cd /home/ubuntu/mobile-backend
+tar -zxf /tmp/mbackend.tar.gz
---
+++ scripts/validate_service.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -fss localhost:8080/messages
