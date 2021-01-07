970a194aee7b389976209d89942e339167aa796e
Rophy Tsai <rophyt@amazon.com>
2021-01-07 14:10:33 +0800

    fix build script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,13 +2,11 @@
 
 set -e
 
-# working directory should be something like:
-# /opt/codedeploy-agent/deployment-root/3820674c-32d1-448e-bdd3-dc57d69af250/d-GMIE7G0B7/deployment-archive/
+# build should be provided by CodeDeploy in files section of appspec.yml
+BUILD_PATH=/tmp/mbackend_build/mbackend.tar.gz
 
 TMP_DIR=/tmp/mobile-backend-$(date +%s)
 mkdir -p "$TMP_DIR"
-tar -zxf mbackend.tar.gz -C "$TMP_DIR"
+tar -zxf "$BUILD_PATH" -C "$TMP_DIR"
 mv /home/ubuntu/mobile-backend /home/ubuntu/mobile-backend-old && mv "$TMP_DIR" /home/ubuntu/mobile-backend
 rm -rf /home/ubuntu/mobile-backend-old
-
-
