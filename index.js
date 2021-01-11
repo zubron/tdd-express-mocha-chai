302ab75b77bbc30ca1c978143bd592bccb46739c
Rophy Tsai <rophyt@amazon.com>
2021-01-11 16:22:04 +0800

    add something

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ var messageServer = require('./src/server');
 messageServer(8080)
     .then(function(server) {
         console.log('Message server listening at %s:%s', server.address().address, server.address().port);
+        console.log('hello world');
     });
